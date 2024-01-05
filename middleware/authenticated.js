69714c7557fbf43e2c181749fb2cc3b56f5f2883
hiepnt2 <hiepnt2@rikkeisoft.com>
2024-01-05 17:31:00 +0700

    side bar

---
+++ middleware/authenticated.js
@@ -0,0 +1,12 @@
+export default function ({ route, app, store, $auth, redirect }) {
+  if (route.path === "/") {
+    // if (!store.$auth.$state.loggedIn) {
+    //   return app.router.replace({
+    //     path: "/login/",
+    //   });
+    // }
+    return app.router.replace({
+      path: "/dashboard",
+    });
+  }
+}
